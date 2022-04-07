33e988ccb18ffdf35ac6ff09e5cb4c028cdd4fce
Jack-Ricketts <zygga.k@gmail.com>
2022-04-07 22:10:06 +0300

    body mass index calculator

---
+++ src/app/components/body/body.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-body',
+  templateUrl: './body.component.html',
+  styleUrls: ['./body.component.css']
+})
+export class BodyComponent implements OnInit {
+  
+  public ugis?:number;
+  public svoris?:number;
+  public kmi:number=0;
+  public info:string='';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  public skaiciuoti(){
+    if (this.ugis!=null && this.svoris!=null){
+      this.kmi= Math.round(this.svoris / (this.ugis/100)**2) ;
+      if (this.kmi<25) 
+        this.info="Sveikiname Jūsų svoris normalus.";
+      if (this.kmi>=25 && this.kmi<30) 
+        this.info="Jūs turėtumėte maitintis sveikiau, KMI šiektiek didesnis";
+      if (this.kmi>=30)
+         this.info="Jūs turėtumėte sunerimti, KMI perdidelis"; 
+    } 
+  } 
+
+}
